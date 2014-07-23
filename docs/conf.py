8af3db2c30d983391726e37e1f74a97a37373f10
vunhat_minh <vunhat_minh@dwango.co.jp>
2014-07-23 12:45:39 +0900

    jinja2 docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-sys.path.append(os.path.abspath('_themes'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -99,9 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-html_theme_path = ['_themes']
-html_theme = 'flask'
+html_theme = 'scrolls'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
