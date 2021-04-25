c1e7882f1318df04cdf498f50bdf078f11746e11
Parsa Torbati <ptorbatii@gmail.com>
2021-04-25 18:54:36 +0430

    apply requested changes to maintain Python 2 compatibility

--- rainbowstream/py3patch.py
+++ rainbowstream/py3patch.py
@@ -4,18 +4,20 @@ import sys
 if sys.version[0] == "2":
     from HTMLParser import HTMLParser
     from urllib2 import URLError
+    unescape = HTMLParser().unescape
 else:
     from html.parser import HTMLParser
     from urllib.error import URLError
+    # HTMLParser().unescape is deprecated since Python 3.4 and is unsafe.
+    # you should use html.unescape instead, like you've said
+    from html import unescape
 
 # unescape = HTMLParser().unescape
 # According to https://github.com/python/cpython/blob/master/Lib/html/parser.py#L547 ,
 # in python 3.5 maybe I should use
 # from html import unescape
 # ~~but it is a far-future story:)~~ no longer. 
-# HTMLParser().unescape is deprecated since Python 3.4 and is unsafe.
-# you should use html.unescape instead, like you've said
-from html import unescape
+
 
 
 # Function compatibility
